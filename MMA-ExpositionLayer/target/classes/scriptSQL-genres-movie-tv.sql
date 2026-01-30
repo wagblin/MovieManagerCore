c4c1648ad0c0bbd5f152ac871a4fa7b909c945f3
Christophe Blin <wagblin@icloud.com>
2026-01-30 15:52:34 +0100

    upgrade jdk21 with copilot from mac16

---
+++ MMA-ExpositionLayer/target/classes/scriptSQL-genres-movie-tv.sql
@@ -0,0 +1,39 @@
+insert into genre_movie(id, name) values(28,'Action');
+insert into genre_movie(id, name) values(12,'Aventure');
+insert into genre_movie(id, name) values(16,'Animation');
+insert into genre_movie(id, name) values(35,'Comédie');
+insert into genre_movie(id, name) values(80,'Crime');
+insert into genre_movie(id, name) values(99,'Documentaire');
+insert into genre_movie(id, name) values(18,'Drame');
+insert into genre_movie(id, name) values(10751,'Familial');
+insert into genre_movie(id, name) values(14,'Fantastique');
+insert into genre_movie(id, name) values(36,'Histoire');
+insert into genre_movie(id, name) values(27,'Horreur');
+insert into genre_movie(id, name) values(10402,'Musique');
+insert into genre_movie(id, name) values(9648,'Mystère');
+insert into genre_movie(id, name) values(10749,'Romance');
+insert into genre_movie(id, name) values(878,'Science-Fiction');
+insert into genre_movie(id, name) values(10770,'Téléfilm');
+insert into genre_movie(id, name) values(53,'Thriller');
+insert into genre_movie(id, name) values(10752,'Guerre');
+insert into genre_movie(id, name) values(37,'Western');
+
+INSERT INTO genre_tv (id, name)
+VALUES
+    (10759, 'Action & Adventure'),
+    (16, 'Animation'),
+    (35, 'Comédie'),
+    (80, 'Crime'),
+    (99, 'Documentaire'),
+    (18, 'Drame'),
+    (10751, 'Familial'),
+    (10762, 'Kids'),
+    (9648, 'Mystère'),
+    (10763, 'News'),
+    (10764, 'Reality'),
+    (10765, 'Science-Fiction & Fantastique'),
+    (10766, 'Soap'),
+    (10767, 'Talk'),
+    (10768, 'War & Politics'),
+    (37, 'Western');
+
